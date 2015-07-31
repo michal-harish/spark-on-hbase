e8b73fe9dd69885f463b61b8b349ba4d34ce0047
Michal Harish <michal.haris@imagini.net>
2015-07-31 17:23:02 +0100

    Added ByteUtils hexadecimal converters and HKeySpaceHEX

--- src/test/scala/org/apache/spark/hbase/ByteUtilsTest.scala
+++ src/test/scala/org/apache/spark/hbase/ByteUtilsTest.scala
@@ -11,6 +11,6 @@ class ByteUtilsTest extends FlatSpec with Matchers {
   it should "be correctly reverse bytes to the original hexadecimal" in {
     val input = "----020ac416f90d91cffc09b56a9e7aea0420e0cf59----"
     val b = ByteUtils.parseRadix16(input.getBytes, 4, 40)
-    ByteUtils.toRadix16(b, 0, 40) should be("020ac416f90d91cffc09b56a9e7aea0420e0cf59")
+    ByteUtils.toRadix16(b, 0, 20) should be("020ac416f90d91cffc09b56a9e7aea0420e0cf59")
   }
 }
